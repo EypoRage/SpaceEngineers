04e848566e83689ac3e3bb639cec69e318ac651e
Tobias Krammer <tobias.krammer.117@gmail.com>
2019-05-16 13:21:25 +0200

    Implemented anchor controll
    Implemented anchor controll (mind twister lol)

--- AnchorControll/AnchorControll/Program.cs
+++ AnchorControll/AnchorControll/Program.cs
@@ -20,57 +20,120 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
+        IMyBlockGroup groupLandingGears;
+        List<IMyLandingGear> landingGears;
+
+        IMyBlockGroup groupAnchorPistons;
+        List<IMyPistonBase> anchorPistons;
+        
 
         public Program()
         {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set RuntimeInfo.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;
+
+            // only fetch the objects on start to reduce processing complexity
+            groupLandingGears = GridTerminalSystem.GetBlockGroupWithName("Bohrinsel Anker Fuesse") as IMyBlockGroup;
+            landingGears = new List<IMyLandingGear>();
+            groupLandingGears.GetBlocksOfType(landingGears);
+
+            groupAnchorPistons = GridTerminalSystem.GetBlockGroupWithName("Bohrinsel Anker Kolben") as IMyBlockGroup;
+            anchorPistons = new List<IMyPistonBase>();
+            groupAnchorPistons.GetBlocksOfType(anchorPistons);
         }
 
         public void Save()
         {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
+        
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from. Be aware that the
-            // updateSource is a  bitfield  and might contain more than 
-            // one update type.
-            // 
-            // The method itself is required, but the arguments above
-            // can be removed if not needed.
+
+            if (argument.Equals("lockToGround")) {
+                lockToGround();
+
+            } else if (argument.Equals("unlockFromGround")) {
+                unlockFromGround();
+
+            } else {
+                Runtime.UpdateFrequency = UpdateFrequency.None;
+
+            }
+
+            
+
+
+            
         }
+
+
+        void lockToGround() {
+
+            bool isFullyLocked = true;
+
+            for (int i = 0; i < landingGears.Count; i++)
+            {
+
+                if (!landingGears[i].IsLocked)
+                {
+
+                    landingGears[i].AutoLock = true;
+                    anchorPistons[i].MaxLimit = 15f;
+                    anchorPistons[i].Velocity = 0.1f;
+                    isFullyLocked = false;
+
+
+                }
+                else if (landingGears[i].IsLocked)
+                {
+                    anchorPistons[i].Velocity = 0.0f;
+                }
+
+            }
+
+            if (isFullyLocked)
+            {
+                Runtime.UpdateFrequency = UpdateFrequency.None;
+            }
+
+        }
+
+
+
+
+
+        void unlockFromGround()
+        {
+
+            bool isFullyUnlocked = true;
+
+            for (int i = 0; i < landingGears.Count; i++)
+            {
+
+                if (!landingGears[i].IsLocked)
+                {
+
+
+                }
+                else if (landingGears[i].IsLocked)
+                {
+                    landingGears[i].AutoLock = false;
+                    landingGears[i].Unlock();
+                    anchorPistons[i].MinLimit = 0f;
+                    anchorPistons[i].Velocity = -0.1f;
+                    isFullyUnlocked = false;
+                }
+
+            }
+
+            if (isFullyUnlocked)
+            {
+                Runtime.UpdateFrequency = UpdateFrequency.None;
+            }
+
+        }
+
+
+
     }
 }
